cbdca7574c9b380d1bbddc6aca818d96244dbf6e
elis <141450459+elishere404@users.noreply.github.com>
2024-12-07 12:44:25 +0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Profile';
 import Projects from './pages/Projects';
 import Hobby from './pages/Hobby/Hobby';
-import HobbyPage from './pages/Hobby/HobbyPage'; // Single dynamic page for hobby details
+import HobbyPage from './pages/Hobby/HobbyPage'; 
 
 const App = () => {
   return (
@@ -12,7 +12,7 @@ const App = () => {
         <Route path="/" element={<Home />} />
         <Route path="/projects" element={<Projects />} />
         <Route path="/hobby" element={<Hobby />} />
-        <Route path="/hobby/:id" element={<HobbyPage />} /> {/* Dynamic Route */}
+        <Route path="/hobby/:id" element={<HobbyPage />} /> 
       </Routes>
     </Router>
   );
